8b7ce37437828590b5f514be871944f84b750c3b
Andrew Johnson <lajandy@cpan.org>
2012-06-13 20:34:15 +0000

    Import of LAJANDY/HeliosX-Logger-Log4perl-0.05_2431 from CPAN.
    gitpan-cpan-distribution: HeliosX-Logger-Log4perl
    gitpan-cpan-version:      0.05_2431
    gitpan-cpan-path:         LAJANDY/HeliosX-Logger-Log4perl-0.05_2431.tar.gz
    gitpan-cpan-author:       LAJANDY
    gitpan-cpan-maturity:     developer

--- lib/HeliosX/Logger/Log4perl.pm
+++ lib/HeliosX/Logger/Log4perl.pm
@@ -10,7 +10,7 @@ use Log::Log4perl;
 use Helios::LogEntry::Levels qw(:all);
 use Helios::Error::LoggingError;
 
-our $VERSION = '0.03_4931';
+our $VERSION = '0.05_2431';
 
 =head1 NAME
 
@@ -23,6 +23,7 @@ HeliosX::Logger::Log4perl - Helios::Logger subclass implementing logging to Log4
  log4perl_conf=/path/to/log4perl.conf
  log4perl_category=logging.category
  log4perl_watch_interval=10
+ log4perl_priority_threshold=6
  
  # log4perl supports lots of options, so you can get creative
  # e.g. log everything to log4perl, 
@@ -73,13 +74,42 @@ particular Helios services, allowing certain services to log to their own
 category but others to default to the global one.  
 
 If log4perl_category is not specified, the Log4perl category will default to 
-the service name/jobtype (from getJobType()).
+the name of your service class.
 
 =head2 log4perl_watch_interval
 
 If specified, Log4perl will reread the log4perl_conf file after the given 
 number of seconds and update its configuration accordingly.  If this isn't 
-specified, any changes to your conf file will require a service restart.
+specified, any changes to your conf file will require you to restart your 
+service daemon to pick up the new configuration.
+
+=head2 log4perl_priority_threshold
+
+Just like log_priority_threshold, but for syslogd.  If you just want to log 
+messages of a certain priority or higher, you can set a numeric value for 
+log4perl_priority_threshold and any log messages of a higher value (lower 
+priority) will be discarded.  The priority levels are defined in 
+Helios::LogEntry::Levels.
+
+=head3 Priority Translation
+
+Helios was originally developed using Sys::Syslog as its primary logging 
+system.  It eventually developed its own internal logging subsystem, and 
+Helios 2.30 added the Helios::Logger interface to further modularize Helios's 
+logging capabilities and make it useful in more environments.  Due to this 
+history, however, Helios defines 8 logging priorities versus Log4perl's 5.  
+HeliosX::Logger::Log4perl translates on-the-fly several of the priority levels 
+defined in Helios::LogEntry::Levels to Log4perl's levels:
+
+ numeric value  Helios::LogEntry::Levels   Log::Log4perl::Level
+ 0              LOG_EMERG                  $FATAL
+ 1              LOG_ALERT                  $FATAL
+ 2              LOG_CRIT                   $FATAL
+ 3              LOG_ERR                    $ERROR
+ 4              LOG_WARNING                $WARN
+ 5              LOG_NOTICE                 $INFO
+ 6              LOG_INFO                   $INFO
+ 7              LOG_DEBUG                  $DEBUG
 
 =head1 IMPLEMENTED METHODS
 
@@ -113,25 +143,6 @@ sub init {
 The logMsg() method logs the given message to the configured log4perl_category with the given 
 $priority_level.
 
-=head3 Priority Translation
-
-Helios was originally developed using Sys::Syslog as its primary logging 
-system.  It eventually developed its own internal logging subsystem, and 
-Helios 2.30 added the Helios::Logger interface to further modularize Helios's 
-logging capabilities and make it useful in more environments.  Due to this 
-history, however, Helios defines 8 logging priorities versus Log4perl's 5.  
-HeliosX::Logger::Log4perl translates on-the-fly several of the priority levels 
-defined in Helios::LogEntry::Levels to Log4perl's levels:
-
- Helios::LogEntry::Levels   Log::Log4perl::Level
- LOG_EMERG                  $FATAL
- LOG_ALERT                  $FATAL
- LOG_CRIT                   $FATAL
- LOG_ERR                    $ERROR
- LOG_WARNING                $WARN
- LOG_NOTICE                 $INFO
- LOG_INFO                   $INFO
- LOG_DEBUG                  $DEBUG
 
 =cut
 
@@ -147,6 +158,14 @@ sub logMsg {
     unless ( Log::Log4perl->initialized() ) {
         $self->init();
     }
+
+	# if syslog_priority_threshold is set & this priority 
+	# isn't as bad as that, don't bother doing any syslog stuff
+	if ( defined($config->{log4perl_priority_threshold}) &&
+		$level > $config->{log4perl_priority_threshold} )
+	{
+		return;
+	}
 
     # if a l4p category was specified, get a logger for it
     # otherwise, get a logger for the jobtype
@@ -217,7 +236,7 @@ Andrew Johnson, E<lt>lajandy at cpan dotorgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2009-11 by Andrew Johnson
+Copyright (C) 2009-12 by Andrew Johnson
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.0 or,
